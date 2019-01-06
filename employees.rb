22095e09f91789d2d171244e3908c1bd1165b9f0
Barry <bdc1130@yahoo.com>
2019-01-06 13:11:08 -0600

    listing employees in a hash

--- employees.rb
+++ employees.rb
@@ -1,7 +1,15 @@
-employee1 = ["Fred", "Flinstone", 80000, true]
+#array
+# employee1 = ["Fred", "Flinstone", 80000, true]
 
-employee2 = ["Barney", "Wruble", 70000, false]
+# employee2 = ["Barney", "Wruble", 70000, false]
 
-p employee1[0] + " " + employee1[1] + " makes $" + employee1[2].to_s + " per year."
+# p employee1[0] + " " + employee1[1] + " makes $" + employee1[2].to_s + " per year."
 # p employee2[0] + " " + employee2[1] + " makes $" + employee2[2].to_s + " per year"
-p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year."
+# p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year."
+
+#hash
+employee1 = {"first_name" => "Fred", "last_name" => "Flinstone", "salary" => 80000, "active" => true}
+employee2 = {"first_name" => "Barney", "last_name" => "Wruble", "salary" => 70000, "active" => false}
+
+p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year."
+p "#{employee2['first_name']} #{employee2['last_name']} makes $#{employee2['salary']} per year."
