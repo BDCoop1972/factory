79188313ea0463585c17d728196ee980fb23869c
Barry <bdc1130@yahoo.com>
2019-01-06 14:51:28 -0600

    add attr_reader and attr_writer

--- employees.rb
+++ employees.rb
@@ -32,27 +32,14 @@ employee3 = {
 # p "#{employee3[:first_name]} #{employee3[:last_name]} makes $#{employee3[:salary]} per year."
 
 class Employee
+  attr_reader :first_name, :last_name, :salary
+  attr_writer :first_name
+
   def initialize(input_first_name, input_last_name, input_salary, input_active)
     @first_name = input_first_name
     @last_name = input_last_name
     @salary = input_salary
     @active = input_active
-  end
-
-  def first_name
-    @first_name  
-  end
-
-  def first_name=(input_first_name)
-    @first_name = input_first_name    
-  end
-
-  def last_name
-    @last_name    
-  end
-
-  def salary
-    @salary
   end
 
   def print_info
@@ -71,7 +58,7 @@ employee2 = Employee.new("Barney", "Wruble", 70000, false)
 # p employee1.salary
 # p employee1.first_name
 # p employee1.last_name
-# employee1.print_info
+employee1.print_info
 # employee1.give_annual_raise
 # employee1.print_info
 p employee1.first_name
