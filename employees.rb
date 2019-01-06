791a0c180f2ae999896939d5a6669a24ce355978
Barry <bdc1130@yahoo.com>
2019-01-06 12:51:30 -0600

    printing employees with concatenation and interpolation

--- employees.rb
+++ employees.rb
@@ -1,3 +1,7 @@
 employee1 = ["Fred", "Flinstone", 80000, true]
 
 employee2 = ["Barney", "Wruble", 70000, false]
+
+p employee1[0] + " " + employee1[1] + " makes $" + employee1[2].to_s + " per year."
+# p employee2[0] + " " + employee2[1] + " makes $" + employee2[2].to_s + " per year"
+p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2].to_s} per year."
