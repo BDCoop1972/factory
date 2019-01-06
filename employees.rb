ea95fa5158a2f7ee17e958f114aa2089632b843b
Barry <bdc1130@yahoo.com>
2019-01-06 14:11:41 -0600

    creating a class

--- employees.rb
+++ employees.rb
@@ -27,6 +27,35 @@ employee3 = {
   active: false
 }
 
-p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year."
-p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
-p "#{employee3[:first_name]} #{employee3[:last_name]} makes $#{employee3[:salary]} per year."
+# p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year."
+# p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
+# p "#{employee3[:first_name]} #{employee3[:last_name]} makes $#{employee3[:salary]} per year."
+
+class Employee
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+  end
+
+
+  def print_info
+    p "#{@first_name} #{@last_name} makes #{@salary} per year."
+  end
+
+  def give_annual_raise
+    @salary = @salary * 1.05
+    
+  end
+end
+
+employee1 = Employee.new("Fred", "Flintsone", 80000, true)
+employee2 = Employee.new("Barney", "Wruble", 70000, false)
+
+# p employee1.salary
+# p employee1.first_name
+# p employee1.last_name
+employee1.print_info
+employee1.give_annual_raise
+employee1.print_info
