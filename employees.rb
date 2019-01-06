31b65978cae1351453f4ce6bc64c0f3b46e40c3d
Barry <bdc1130@yahoo.com>
2019-01-06 12:54:30 -0600

    removed .to_s in interpolation due to not necessary

--- employees.rb
+++ employees.rb
@@ -4,4 +4,4 @@ employee2 = ["Barney", "Wruble", 70000, false]
 
 p employee1[0] + " " + employee1[1] + " makes $" + employee1[2].to_s + " per year."
 # p employee2[0] + " " + employee2[1] + " makes $" + employee2[2].to_s + " per year"
-p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2].to_s} per year."
+p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year."
